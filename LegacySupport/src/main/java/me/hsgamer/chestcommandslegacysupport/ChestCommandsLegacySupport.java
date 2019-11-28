e0fa204997299850d5eba31a0b2306340f1d5791
HSGamer <huynhqtienvtag@gmail.com>
2019-11-28 23:32:27 +0700

    working for legacy support

---
+++ LegacySupport/src/main/java/me/hsgamer/chestcommandslegacysupport/ChestCommandsLegacySupport.java
@@ -0,0 +1,17 @@
+package me.hsgamer.chestcommandslegacysupport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChestCommandsLegacySupport extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
